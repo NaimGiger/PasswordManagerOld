473d5da127daba7c3dfc9e046416961c56eff1ab
NaimGiger <naim_giger@sluz.ch>
2022-12-16 10:50:05 +0100

    create basic UserInterface

--- src/main/java/tech/bison/trainee2021/UserInterface.java
+++ src/main/java/tech/bison/trainee2021/UserInterface.java
@@ -1,7 +1,22 @@
 package tech.bison.trainee2021;
 
+import java.util.Scanner;
+
 public class UserInterface {
     public void run() {
-        // TODO: Define UI
+        System.out.println("PASSWORD MANAGER");
+        String input = getInput("User > ");
+        System.out.println(generateMessage(input));
+    }
+
+    // This method cant be tested bc it is the interface to the user
+    String getInput(String prompt) {
+        System.out.println(prompt);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    String generateMessage(String userInput) {
+        return  "";
     }
 }
